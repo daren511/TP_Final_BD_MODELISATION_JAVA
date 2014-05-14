5fde9612a61d4e90e6e58ab70ee4cb70716f2adc
Daren-Ken St-laurent <daren511@hotmail.com>
2014-05-14 13:19:33 -0400

    Francis - Inscription work into sql
    need to rewrite html page when inserted though

--- Servlet/DebarasBoileau/src/java/Inscription/Inscription.java
+++ Servlet/DebarasBoileau/src/java/Inscription/Inscription.java
@@ -1,8 +1,8 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+* To change this license header, choose License Headers in Project Properties.
+* To change this template file, choose Tools | Templates
+* and open the template in the editor.
+*/
 
 package Inscription;
 
@@ -15,6 +15,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import oracle.jdbc.*;
 import oracle.jdbc.pool.*;
+import java.sql.*;
 
 /**
  *
@@ -22,7 +23,7 @@ import oracle.jdbc.pool.*;
  */
 @WebServlet(name = "Inscription", urlPatterns = {"/Inscription"})
 public class Inscription extends HttpServlet {
-
+    
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
      * methods.
@@ -32,11 +33,12 @@ public class Inscription extends HttpServlet {
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
+    
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
-           
+            
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             ecrireTete(out,"Inscription");
@@ -65,14 +67,16 @@ public class Inscription extends HttpServlet {
     }
     
     private void ecrireTete(PrintWriter writer, String Titre){
-            writer.println("<head>");
-            writer.println("<meta charset=\"utf-8\" />");
-            writer.println("<title>"+Titre+"</title>"); 
-            writer.println("<link rel=\"stylesheet\" type=\"text/css\" href=\"WebApp.css\">");
-            writer.println("</head>");
+        writer.println("<head>");
+        writer.println("<meta charset=\"utf-8\" />");
+        writer.println("<title>"+Titre+"</title>");
+        writer.println("<link rel=\"stylesheet\" type=\"text/css\" href=\"WebApp.css\">");
+        writer.println("</head>");
     }
     
-
+    
+    
+    
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
      * Handles the HTTP <code>GET</code> method.
@@ -88,7 +92,7 @@ public class Inscription extends HttpServlet {
         processRequest(request, response);
         System.out.println("GET");
     }
-
+    
     /**
      * Handles the HTTP <code>POST</code> method.
      *
@@ -98,26 +102,74 @@ public class Inscription extends HttpServlet {
      * @throws IOException if an I/O error occurs
      */
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+    protected void doPost(
+            HttpServletRequest request , HttpServletResponse response )
+            throws ServletException, IOException
+    {
+        // Recup des params de la page
         String username = request.getParameter("User");
         String password = request.getParameter("Password");
-        String lastName = request.getParameter("Nom");
-        String firstName = request.getParameter("Prenom");
-        ConnectionOracle conn = new ConnectionOracle();
+        String nom = request.getParameter("Nom");
+        String prenom = request.getParameter("Prenom");
+        int nombreecus = 500;
         
-        conn.connecter();
+        
+        //avertit le navigateur qu'il va recevoir du code HTML
+        response.setContentType("text/html;charset=UTF-8");
+        
+        //Flux ecriture
+        PrintWriter out = response.getWriter();
+        
+        try
+        {
+            // Write Head
+            ecrireTete(out,"Inscription");
+            
+            //Connexion
+            ConnectionOracle oradb = new ConnectionOracle();
+            oradb.connecter();
+            
+            // Insertion joueur
+            String sqlins = "insert into Joueurs(NOMUSAGER,NOMJOUEUR,PRENOM,MOTDEPASSE,ECUSJOUEURS) values (?,?,?,?,?)";
+            
+            try
+            {
+                PreparedStatement stmins =
+                        oradb.getConnexion().prepareStatement(sqlins);
+                
+                //Affectation
+                stmins.setString(1,username);
+                stmins.setString(2, nom);
+                stmins.setString(3,prenom);
+                stmins.setString(4, password);
+                stmins.setInt(5, nombreecus);
+                stmins.executeUpdate();
+                stmins.close();
+            }
+            catch(SQLException se)
+            {
+                System.err.println(se.getMessage());
+            }
+            
+        }
+        finally
+        {
+            out.close();
+        }
+        
+        //confirmation test
+        out.println("<p>Vous avez bien ajoute le joueur suivant :</p>");
+        out.println("<p>" + username+" "+password+" "+prenom+" "+nom+"</p>");
+        
+        /**
+         * Returns a short description of the servlet.
+         *
+         * @return a String containing servlet description
+         */
+        //@Override
+        //public String getServletInfo() {
+        //    return "Short description";
+        //}// </editor-fold>
         
     }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
-
 }
